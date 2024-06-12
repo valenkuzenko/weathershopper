1c7b2e9564dd9f9d254921aaaf6a3a4a0733c459
valen <valentyna@taxer.ua>
2024-06-12 20:47:50 +0300

    removed error from verifyPaymentResult

--- src/pages/confirmation-page.po.ts
+++ src/pages/confirmation-page.po.ts
@@ -1,4 +1,6 @@
+import { assert } from "console";
 import { PageAbstract } from "./page-abstract.po";
+import { expect } from "playwright/test";
 
 export class ConfirmationPage extends PageAbstract {
     async waitForUrl(): Promise<void> {
@@ -7,10 +9,6 @@ export class ConfirmationPage extends PageAbstract {
 
     async verifyPaymentResult(expectedResult: string): Promise<void> {
         const confirmationText = await this.page.locator('h2').innerText();
-        // we need to take into account the 5% probability of getting fail result as this behaviour is also correct, the test must pass
-        const failText = "PAYMENT FAILED";
-        if (confirmationText !== expectedResult && confirmationText !== failText) {
-            throw new Error(`Expected payment result to be '${expectedResult}' or '${failText}', but got '${confirmationText}'`);
-        }
+        expect(confirmationText).toEqual(expectedResult);
     }
 }
